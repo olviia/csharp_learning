af578a39dfc0ad0300f9e1752e5fd98042992b69
olviia <olviia.stroivans@gmail.com>
2022-04-13 15:23:46 +0200

    full project and  a + b = sum

---
+++ learn/learn/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, sum;
+            Console.WriteLine("Enter the first number");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            sum = a + b;
+            Console.WriteLine($"The sum is {sum}");
+            Console.ReadLine();
+        }
+    }
+}
