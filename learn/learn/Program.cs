f6928962f3b677b6adb64429130e70060913b493
olviia <olviia.stroivans@gmail.com>
2022-04-14 13:01:10 +0200

    enter month number and get month name

--- learn/learn/Program.cs
+++ learn/learn/Program.cs
@@ -6,15 +6,32 @@ namespace learn
     {
         static void Main(string[] args)
         {
-            double a, b, sum;
-            Console.WriteLine("Enter the first number");
-            a = Convert.ToDouble(Console.ReadLine());
+            string[] months = { "January",
+                                "Fabryary",
+                                "March", 
+                                "April", 
+                                "May", 
+                                "June", 
+                                "July", 
+                                "August", 
+                                "September", 
+                                "October", 
+                                "November", 
+                                "December"};
+            int number;
 
-            Console.WriteLine("Enter the second number");
-            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the number of month");
+            number = Convert.ToInt32(Console.ReadLine());
 
-            sum = a + b;
-            Console.WriteLine($"The sum is {sum}");
+            if(number <= 12 && number >= 1)
+            {
+                Console.WriteLine(months[number - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Input was wrong");
+            }
+
             Console.ReadLine();
         }
     }
