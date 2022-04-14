b2a6671a7ad1edf4e7db76f49f87f94356ec800f
olviia <olviia.stroivans@gmail.com>
2022-04-14 16:26:46 +0200

    check last digit of number

--- learn/learn/Program.cs
+++ learn/learn/Program.cs
@@ -6,31 +6,52 @@ namespace learn
     {
         static void Main(string[] args)
         {
-            string[] months = { "January",
-                                "Fabryary",
-                                "March", 
-                                "April", 
-                                "May", 
-                                "June", 
-                                "July", 
-                                "August", 
-                                "September", 
-                                "October", 
-                                "November", 
-                                "December"};
             int number;
+            int remainder;
 
-            Console.WriteLine("Enter the number of month");
+            Console.WriteLine("Enter the number");
             number = Convert.ToInt32(Console.ReadLine());
 
-            if(number <= 12 && number >= 1)
+            remainder = number % 10;
+
+            switch (remainder)
             {
-                Console.WriteLine(months[number - 1]);
+                case 7:
+                    Console.WriteLine("Number is uneven, finished with 7");
+                    break;
+                case 1:
+                    Console.WriteLine("Number is uneven, finished with 1");
+                    break;
+                case 3:
+                    Console.WriteLine("Number is uneven, finished with 3");
+                    break;
+                case 5:
+                    Console.WriteLine("Number is uneven, finished with 5");
+                    break;
+                case 9:
+                    Console.WriteLine("Number is uneven, finished with 9");
+                    break;
+                case 2:
+                    Console.WriteLine("Number is even, finished with not 7, finished with 2");
+                    break;
+                case 4:
+                    Console.WriteLine("Number is even, finished with not 7, finished with 4");
+                    break;
+                case 6:
+                    Console.WriteLine("Number is even, finished with not 7, finished with 6");
+                    break;
+                case 8:
+                    Console.WriteLine("Number is even, finished with not 7, finished with 8");
+                    break;
+                case 0:
+                    Console.WriteLine("Number is even, finished with not 7, finished with 0");
+                    break;
+                default:
+                    Console.WriteLine("Something goes wrong");
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Input was wrong");
-            }
+
+
 
             Console.ReadLine();
         }
