e45637a0a5c6988914392383404a78cf2e3066f4
olviia <olviia.stroivans@gmail.com>
2022-04-15 13:16:09 +0200

    updated time of year output

--- learn/learn/Program.cs
+++ learn/learn/Program.cs
@@ -6,18 +6,18 @@ namespace learn
     {
         static void Main(string[] args)
         {
-            string[] months = { "January",
-                                "Fabryary",
-                                "March", 
-                                "April", 
-                                "May", 
-                                "June", 
-                                "July", 
-                                "August", 
-                                "September", 
-                                "October", 
-                                "November", 
-                                "December"};
+            string[] months = { "January - month of winter",
+                                "Fabryary - month of winter",
+                                "March - month of spring",
+                                "April - month of spring",
+                                "May - month of spring",
+                                "June - month of summer",
+                                "July - month of summer",
+                                "August - month of summer",
+                                "September - month of autumn",
+                                "October - month of autumn",
+                                "November - month of autumn",
+                                "December - month of winter"};
             int number;
 
             Console.WriteLine("Enter the number of month");
