472381593b2f40aef4199da81c2b86e6bff85d11
olviia <olviia.stroivans@gmail.com>
2022-04-15 14:43:12 +0200

    comparing digits of 4-digit number

--- learn/learn/Program.cs
+++ learn/learn/Program.cs
@@ -6,42 +6,70 @@ namespace learn
     {
         static void Main(string[] args)
         {
-            //Дано двузначное число.Определить: 
-            // а) какая из его цифр больше, первая или вторая;
-            // б) одинаковы ли его цифры. 
+            /*Дано четырехзначное число.Определить: 
+            а) равна ли сумма двух первых его цифр сумме двух его  последних цифр;
+            б) кратна ли трем сумма его цифр;
+            в) кратно ли четырем произведение его цифр;
+            г) кратно ли произведение его цифр числу а.
+            */
 
+            int input_number;
+            int a_number;
+            int first_number;
+            int second_number;
+            int third_number;
+            int forth_number;
 
-            int number;
-            int remainder;
-            int divided;
+            Console.WriteLine("Enter the 4-digits number");
+            input_number = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the 2-digits number");
-            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another number");
+            a_number = Convert.ToInt32(Console.ReadLine());
 
-            remainder = number % 10;
-            divided = number / 10;
-
-            if (number > 99 || number < 10)
+            if (input_number>999  || input_number < 10000)
             {
-                Console.WriteLine("Wrong number");
-            }
-            else
-            {
-                Console.WriteLine($"First digit is {divided}");
-                Console.WriteLine($"Second digit is {remainder}");
-                if (divided > remainder)
+                forth_number = input_number % 10;
+                third_number = ((input_number - forth_number) / 10) % 10;
+                second_number = ((input_number - forth_number - third_number * 10) / 100) % 10;
+                first_number = ((input_number - forth_number - third_number * 10 - second_number * 100) / 1000) % 10;
+
+                if(first_number+second_number == third_number + forth_number)
                 {
-                    Console.WriteLine($"{divided} is bigger then {remainder}");
+                    Console.WriteLine($"sum of first and second digits equals sum of third and forth digits");
                 }
-                else if (divided < remainder)
+                else
                 {
-                    Console.WriteLine($"{divided} is less then {remainder}");
+                    Console.WriteLine($"sum of first and second digits not equals sum of third and forth digits");
+                }
+
+                if((first_number+second_number+third_number+forth_number) % 3 == 0)
+                {
+                    Console.WriteLine($"sum of all digits is multiple of 3");
                 }
                 else 
                 {
-                    Console.WriteLine($"{divided} equals {remainder}");
+                    Console.WriteLine($"sum of all digits is not multiple of 3");
+                }
+
+                if((first_number*second_number*third_number*forth_number) % 4 == 0)
+                {
+                    Console.WriteLine($"product of all digits is multiple of 4");
+                }
+                else 
+                {
+                    Console.WriteLine($"product of all digits is not multiple of 4");
+                }
+
+                if((first_number*second_number*third_number*forth_number) % a_number == 0)
+                {
+                    Console.WriteLine($"product of all digits is multiple of {a_number}");
+                }
+                else 
+                {
+                    Console.WriteLine($"product of all digits is not multiple of {a_number}");
                 }
             }
+
             Console.ReadLine();
         }
     }
