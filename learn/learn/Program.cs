79583bf99dc6aaea36d769411c658c5f1b869fea
olviia <olviia.stroivans@gmail.com>
2022-04-26 15:24:17 +0200

    used built-in function instead of thinking:))

--- learn/learn/Program.cs
+++ learn/learn/Program.cs
@@ -1,61 +1,34 @@
 ﻿using System;
 
+
 namespace learn
 {
     class Program
     {
         static void Main(string[] args)
         {
-               /* Проверка деления введенного числа на 3.
-                Число делится на 3, если сумма всех цифр делится на 3.
-                Например: число 174 делится на 3, 
-                поскольку сумма его цифр 1 + 7 + 4 = 12 делится на 3.
-                Написать программу для любого вводимого числа от 0 до 1000*/
+           /* При запуске программы, должна отобразиться сегодняшняя дата в формате 14.11.2015.
+            Чем точнее она будет, тем лучше.Подумайте, как можно это реализовать и добиться максимальной точности.
+            Напомню, что системно можно получить количество миллисекунд, прошедших от 01.01.1970 года с момощью метода
+            System.currentTimeInMills();*/
 
-            int input_number;
-            int first_number = 0;
-            int second_number = 0;
-            int third_number = 0;
-            int forth_number = 0;
+            long date;
+            int year;
+            int month;
+            int day;
+            int hour;
+            int minute;
+            int second;
 
             Console.WriteLine("Enter the 1-4 digits number");
-            input_number = Convert.ToInt32(Console.ReadLine());
 
-            if (input_number > 999 || input_number < 10000)
-            {
-                forth_number = input_number % 10;
-                third_number = ((input_number - forth_number) / 10) % 10;
-                second_number = ((input_number - forth_number - third_number * 10) / 100) % 10;
-                first_number = ((input_number - forth_number - third_number * 10 - second_number * 100) / 1000) % 10;
-            }
-            else if (input_number > 99 || input_number < 1000)
-            {
-                third_number = input_number % 10;
-                second_number = ((input_number - third_number) / 10) % 10;
-                first_number = ((input_number - second_number - third_number * 10) / 100) % 10;
-            }
-            else if (input_number > 9 || input_number < 100)
-            {
-                second_number = input_number % 10;
-                first_number = ((input_number - second_number) / 10) % 10;
-            }
-            else if (input_number >= 0 || input_number < 10)
-            {
-                first_number = input_number;
-            }
-            else
-            {
-                Console.WriteLine("Incorrect input");
-            }
+            string timestamp = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff");
 
-            if ((first_number+second_number+third_number+forth_number)%3 == 0)
-            {
-                Console.WriteLine($"{input_number} is multiple of 3");
-            }
-            else
-            {
-                Console.WriteLine($"{input_number} is not multiple of 3");
-            }
+
+            Console.WriteLine($"{timestamp}");
+
+            //date = DateTimeOffset(now).FromUnixTimeMilliseconds();
+            
 
             Console.ReadLine();
         }
