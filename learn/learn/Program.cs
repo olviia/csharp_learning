8cd284e8d21fd7f173b3b0858e3da9fd8a47071e
olviia <olviia.stroivans@gmail.com>
2022-04-28 14:52:59 +0200

    repeated programm

--- learn/learn/Program.cs
+++ learn/learn/Program.cs
@@ -7,36 +7,64 @@ namespace learn
     {
         static void Main(string[] args)
         {
-            /*  Программа считает среднее арифметическое вводимых с клавиатуры чисел. 
-             *  Сначала программа предлагает ввести количество этих чисел, 
-             *  а потом предлагает непосредственно вводить эти числа. 
-             *  Ответ должен быть округлен по правилу округления.
+            /* Задача 11.
+            Придумать алгоритм "старых досовских" программ. Программа может повторять свои действия. >
+            Программа вычисляет степень введенного числа.
+            Введите число: 
+            > 7
+            > Введите степень, которую необходимо вычислить:
+            > 4
+            > 7 в степени 4 равно 2401 
+            Хотите продолжить? Да - введите 1, нет - введите 0
              */
-
-            int amountNums;
-            int sum = 0;
-            decimal result;
-
-            Console.WriteLine("Enter the amount of numbers: ");
-            amountNums = Convert.ToInt32(Console.ReadLine());
-
-
-            for(int i = 1; i<=amountNums; i++)
+            StartProgramm();
+        }
+        static void StartProgramm()
+        {
+            bool repeat = true;
+            while (repeat)
             {
-                Console.WriteLine($"Enter {i} number: ");
-                int temp = Convert.ToInt32(Console.ReadLine());
-
-                sum += temp;
+                PowerOfNumber();
+                repeat = AnswerCheck();
             }
 
-            result = sum / Convert.ToDecimal(amountNums);
-            Console.WriteLine($" {result} ");
-
-
-            Console.WriteLine($"Result is {decimal.Round(result)} ");
-
-
+            Console.WriteLine("Thank you for using this programm!");
             Console.ReadLine();
+        }
+        static void PowerOfNumber()
+        {
+            double number;
+            double power;
+            double result;
+
+            Console.WriteLine("Enter the number:");
+            number = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the power of number:");
+            power = Convert.ToDouble(Console.ReadLine());
+
+            result = Math.Pow(number, power);
+            Console.WriteLine($"{number}^{power}={result}");
+        }
+
+        static bool AnswerCheck()
+        {
+            string answer;
+            Console.WriteLine("Do you want to continue programm? Enter yes or no");
+            answer = Console.ReadLine();
+
+            if(answer == "yes")
+            {
+                return true;
+            } else if(answer == "no")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("You entered incorrect input. Try again");
+                return AnswerCheck();
+            }
         }
     }
 }
